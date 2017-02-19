64d021fed3c601eb9341d9976b676938b7aad15a
Jeremy Jurksztowicz <jjurksztowicz@ehealthinnovation.org>
2017-02-19 11:21:13 -0500

    Initial Commit

---
+++ n_ranges_linear/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  n_ranges_linear
+//
+//  Created by Jeremy Jurksztowicz on 2/19/17.
+//  Copyright © 2017 Jeremy Jurksztowicz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
