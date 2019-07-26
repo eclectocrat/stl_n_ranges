ec12c2184c13c6b2a4b58bbaefc9bc4df53b2cb1
Jeremy Jurksztowicz <jurksztowicz@gmail.com>
2019-07-26 09:51:40 -0400

    Update n-ranges.py

--- python/n-ranges.py
+++ python/n-ranges.py
@@ -17,3 +17,16 @@ _Desc = namedtuple('Desc',
 def _desc(num_elements, num_ranges):
 	return _Desc(num_elements, num_ranges, num_elements // num_ranges, 
 				 _positive_ratio(num_elements % num_ranges, num_ranges))
+
+def _range_index_to_element_index(desc, range_index):
+	assert range_index < desc.num_ranges
+	if desc.ratio.numerator == 0:
+		return range_index * desc.elements_per_range
+	elif range_index == desc.num_ranges:
+		return desc.num_elements
+	else:
+		extra = 0 if (range_index * desc.ratio.numerator 					 
+					 % desc.ratio.denominator < desc.ratio.numerator) else 1
+		return range_index * desc.elements_per_range 						 \
+			+ (range_index * desc.ratio.numerator // desc.ratio.denominator) \
+			+ extra
