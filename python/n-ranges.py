00afd9b8c1e2c6d7d9b7d9515e09b43ffdc75765
Jeremy Jurksztowicz <jurksztowicz@gmail.com>
2019-07-26 09:49:13 -0400

    Update n-ranges.py

--- python/n-ranges.py
+++ python/n-ranges.py
@@ -1,9 +1,19 @@
-def _gcd(n, d):
-    '''comments'''
-    pass
-  
-def positive_ratio(n, d):
-    pass
+from collections import namedtuple
 
-def range_index_to_element():
-    pass
+
+# Private interface:
+
+_Ratio = namedtuple('Ratio', 'numerator denominator')
+
+def _positive_ratio(n, d):
+	assert n > 0 or d > 0
+	gcd = math.gcd(n, d)
+	return _Ratio(n // gcd, d // gcd)
+
+
+_Desc = namedtuple('Desc', 
+				   'num_elements num_ranges elements_per_range ratio')
+
+def _desc(num_elements, num_ranges):
+	return _Desc(num_elements, num_ranges, num_elements // num_ranges, 
+				 _positive_ratio(num_elements % num_ranges, num_ranges))
